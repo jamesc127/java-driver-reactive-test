5b55c651d368fbc98e7e6b52e354c60f42d76547
James Colvin <jamesc127@gmail.com>
2020-02-21 11:59:19 -0600

    initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,42 +2,50 @@ package org.example;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.*;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import reactor.core.publisher.Flux;
 
 public class App 
 {
+    public static Row getFromOtherTable(Integer key, PreparedStatement pState, CqlSession session){
+        BoundStatement bound = pState.bind(key);
+        Row row = session.execute(bound).one();
+        System.out.println(row.getFormattedContents());
+        return row;
+    }
     public static void main( String[] args )
     {
         try (CqlSession session = CqlSession.builder().build()) {
-            System.out.println("Example of a prepared statement with an execution profile");
-            PreparedStatement prep =
-                    session.prepare("SELECT * FROM profile_test.test WHERE pkey = ? AND clustering1 = ? AND clustering2 < ?");
-            BoundStatement bound =
-                    prep.bind("pkey1",1,5).setExecutionProfileName("core");
-
-            ResultSet rs = session.execute(bound);
-            for (Row r : rs) {
-                System.out.println(r.getFormattedContents());
+            System.out.println("Reactive Flux that doesn't blockLast()");
+            List<Integer> keys = new ArrayList<>();
+            for (int i = 0; i < 2; i++) {
+                keys.add(i);
             }
-
-            System.out.println("Example of a simple statement with the default execution profile");
-            SimpleStatement state =
-                    SimpleStatement.builder("SELECT * FROM profile_test.test WHERE pkey = 'pkey2' LIMIT 20").build();
-
-            ResultSet simpRs = session.execute(state);
-            for (Row simpR : simpRs) {
-                System.out.println(simpR.getFormattedContents());
+            List<Row> rowList = new ArrayList<>();
+            List<BoundStatement> bStmnts = new ArrayList<>();
+            PreparedStatement pStatement = session.prepare("SELECT clustering1 FROM profile_test.test WHERE pkey = ?");
+            for (Integer key : keys){
+                BoundStatement query = pStatement.bind(key);
+                bStmnts.add(query);
             }
+            PreparedStatement otherStatement = session.prepare("SELECT clustering1 FROM profile_test.other_test WHERE pkey = ?");
+//            Flux.just(bStmnts.toArray(new BoundStatement[bStmnts.size()]))
+//                    .flatMap(session::executeReactive)
+//                    .doOnNext(rowList::add) //could potentially create a method to make the second call here
+//                    .blockLast(); //or I need to subscribe() here and run the second query
+            Flux.just(bStmnts.toArray(new BoundStatement[bStmnts.size()]))
+                    .flatMap(session::executeReactive)
+                    .subscribe(row -> {
 
-            System.out.println("Example of a search statement with a different execution profile");
-            PreparedStatement prepSearch =
-                    session.prepare("SELECT * FROM profile_test.test WHERE clustering2 > ? AND clustering2 <= ?");
-            BoundStatement boundSearch =
-                    prepSearch.bind(10,12).setExecutionProfileName("search");
+                        System.out.println("entered the subscribe" + );
 
-            ResultSet searchRs = session.execute(boundSearch);
-            for (Row searchR : searchRs) {
-                System.out.println(searchR.getFormattedContents());
+                        BoundStatement otherBound = otherStatement.bind(row.getInt("clustering1"));
+                        Row otherRow = session.execute(otherBound).one();
+                        rowList.add(otherRow);
+                    });
+            for (Row row : rowList){
+                System.out.println(row.getFormattedContents());
             }
         }
     }
--- src/main/java/org/example/Insert.java
+++ src/main/java/org/example/Insert.java
@@ -8,14 +8,27 @@ public class Insert {
     public static void main(String[] args){
         try (CqlSession session = CqlSession.builder().build()) {
             session.execute("CREATE KEYSPACE IF NOT EXISTS profile_test WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'dc1' : 1};");
-            session.execute("CREATE TABLE IF NOT EXISTS profile_test.test (pkey text,clustering1 int,clustering2 int,PRIMARY KEY((pkey),clustering1,clustering2))");
-            session.execute("CREATE SEARCH INDEX IF NOT EXISTS ON profile_test.test WITH COLUMNS *;");
+            session.execute("DROP TABLE IF EXISTS profile_test.test");
+            session.execute("DROP TABLE IF EXISTS profile_test.other_test");
+            session.execute("CREATE TABLE IF NOT EXISTS profile_test.test (pkey int,clustering1 int,clustering2 int,PRIMARY KEY((pkey),clustering1,clustering2))");
+            session.execute("CREATE TABLE IF NOT EXISTS profile_test.other_test (pkey int,clustering1 int,clustering2 int,PRIMARY KEY((pkey),clustering1,clustering2))");
+//            session.execute("CREATE SEARCH INDEX IF NOT EXISTS ON profile_test.test WITH COLUMNS *;");
             PreparedStatement insert = session.prepare("INSERT INTO profile_test.test (pkey, clustering1, clustering2) VALUES (?,?,?)");
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 5000; j++) {
-                    String pkey = "pkey"+i;
-                    BoundStatement bInsert = insert.bind(pkey,i,j);
-                    session.execute(bInsert);
+            PreparedStatement otherInsert = session.prepare("INSERT INTO profile_test.other_test (pkey, clustering1, clustering2) VALUES (?,?,?)");
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 100 ; k++) {
+                        BoundStatement bInsert = insert.bind(i,j,k);
+                        session.execute(bInsert);
+                    }
+                }
+            }
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 100 ; k++) {
+                        BoundStatement oInsert = otherInsert.bind(i,j,k);
+                        session.execute(oInsert);
+                    }
                 }
             }
             session.close();
